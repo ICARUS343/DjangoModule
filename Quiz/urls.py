80f1237debd743d746b634cb6ef3c5c24dd162d1
Icarus <hello@gmail.com>
2020-02-16 12:09:51 -0500

    More to list

--- Quiz/urls.py
+++ Quiz/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('quizApp/', include('quizApp.urls')),
+    path('', include('quizApp.urls')),
     path('admin/', admin.site.urls),
 ]
