889555cf5f64c9ba81044cf22bde3a6ad35e5a2b
Icarus <hello@gmail.com>
2020-02-16 12:53:57 -0500

    Seperated index and question .html

--- quizApp/urls.py
+++ quizApp/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('question/', views.question, name = 'question'),
+    path('question', views.question, name = 'question'),
     # ex: /quizApp/5/
-    #path('<int:quiz_id>/', views.quiz_detail, name='quiz_detail'),
+    path('<int:quiz_id>/', views.index, name='quiz_detail'),
     # ex: /quizApp/5/question/
-    #path('<int:quiz_id>/question/', views.question_detail, name='question_detail'),
+    path('<int:quiz_id>/question/', views.question, name='question'),
     # ex: /quizApp/5/answer/
     #path('<int:quiz_id>/answer/', views.answer_detail, name='answer_detail'),
 ]
