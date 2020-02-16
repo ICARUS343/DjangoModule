d010d89a03fbba33fcc33b5ff5ff8e56be717c9e
Icarus <hello@gmail.com>
2020-02-16 16:40:52 -0500

    Fixed query

--- quizApp/views.py
+++ quizApp/views.py
@@ -13,7 +13,7 @@ def index(request):
 
 
 def question(request):
-    latest_question_list = Question.objects.filter(quiz_foreign_key = request.GET.get('Quiz.quiz_id'))
+    latest_question_list = Question.objects.filter(quiz_foreign_key = request.GET.get(Quiz.quiz_id))
     context = {
         'latest_question_list': latest_question_list,
     }
