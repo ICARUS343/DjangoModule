4ba296a1f8358f37ef1e154c8bf149b780c0883f
Icarus <hello@gmail.com>
2020-02-16 12:07:06 -0500

    More to list

--- quizApp/urls.py
+++ quizApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('quizApp', views.index, name='index'),
+    path('^quizApp', views.index, name='index'),
     path('', views.question, name = 'question'),
     # ex: /quizApp/5/
     #path('<int:quiz_id>/', views.quiz_detail, name='quiz_detail'),
