6769b780db8f0f042f4b6a401af5658630c0c23b
Icarus <hello@gmail.com>
2020-02-16 15:53:52 -0500

    Added to string functions to modules

--- quizApp/models.py
+++ quizApp/models.py
@@ -6,11 +6,17 @@ class Quiz(models.Model):
     quiz_description = models.CharField(max_length=100)
     quiz_difficulty = models.IntegerField(default = 0)
 
+    def __str__(self):
+        return self.quiz_title
+
 class Question(models.Model):
     question_title = models.CharField(max_length=50)
     question_text = models.CharField(max_length=100)
     is_multi_answer = models.BooleanField(default = False)
     quiz_foreign_key = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.question_title
     
 class Answer(models.Model):
     answer_title = models.CharField(max_length=50)
@@ -18,6 +24,9 @@ class Answer(models.Model):
     is_correct_answer = models.BooleanField(default = False)
     number_of_points = models.IntegerField(default = 0)
     question_foreign_key = models.ForeignKey(Question, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.answer_title
 
 
 # Create your models here.
