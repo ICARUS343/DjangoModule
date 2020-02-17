ef8f8feb0074eb96dc7653a1aa8e31bf4e017599
Icarus <hello@gmail.com>
2020-02-16 19:43:33 -0500

    Import

--- quizApp/views.py
+++ quizApp/views.py
@@ -14,7 +14,7 @@ def index(request):
 
 def question(request, quiz_id):
     try:
-        latest_question_list = Question.objects.filter(pk=quiz_id)
+        latest_question_list = Question.objects.filter(quiz_foreign_key=quiz_id)
         context = {'latest_question_list': latest_question_list,}
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
