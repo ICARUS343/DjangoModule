c6879a439a0d6ce775b2cb7e68bb76e736bfb910
Icarus <hello@gmail.com>
2020-02-16 19:35:48 -0500

    Fixed query

--- quizApp/views.py
+++ quizApp/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
 
 from .models import *
 
@@ -7,13 +6,24 @@ from .models import *
 def index(request):
     latest_quiz_list = Quiz.objects.order_by('id')[:5]
     context = {
-	'latest_quiz_list' : latest_quiz_list,
+        'latest_quiz_list': latest_quiz_list,
     }
     return render(request, 'quizApp/quiz.html', context)
 
 
+def question(request, quiz_id):
+    try:
+        latest_question_list = Question.objects.filter(pk=quiz_id)
+        context = {'latest_question_list': latest_question_list,}
+    except Question.DoesNotExist:
+    raise Http404("Question does not exist")
+
+
+    return render(request, 'quizApp/question.html', context)
+
+
 def question(request):
-    latest_question_list = Question.objects.filter(quiz_foreign_key = (request.session['my_key']))
+    latest_question_list = Question.objects.filter(quiz_foreign_key=(request.session['my_key']))
     context = {
         'latest_question_list': latest_question_list,
     }
