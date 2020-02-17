5889be84d4317435ba6ad65772f2d63642db390f
Icarus <hello@gmail.com>
2020-02-16 20:08:19 -0500

    Path fix

--- quizApp/urls.py
+++ quizApp/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from django.shortcuts import render
 
 from . import views
-
+app_name = 'quizApp';
 urlpatterns = [
-    path('', views.index, name='quiz'),
-    path('<int:quiz_id>/', views.question, name = 'question'),
+
+    path('', views.index.as_view(), name='index'),
+    path('<int:pk>/', views.question().as_view(), name='question'),
+
+
+
     # ex: /quizApp/5/
     #path('<int:quiz_id>/question', views.question, name='question'),
     # ex: /quizApp/5/question/
@@ -13,3 +17,5 @@ urlpatterns = [
     # ex: /quizApp/5/answer/
     #path('<int:quiz_id>/answer/', views.answer_detail, name='answer_detail'),
 ]
+
+
