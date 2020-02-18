fd5db7dfd7e5e8bb4c2aeb56bc2b832143b7e993
Icarus <hello@gmail.com>
2020-02-18 12:36:58 -0500

    Registration

--- user/views.py
+++ user/views.py
@@ -7,10 +7,10 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def login(request):
-    return render(request, 'registration/index.html')
+    return render(request, 'registration/login.html')
 
 
 def register(request):
     form = UserCreationForm()
     context = {'form' : form}
-    return render(request, 'registration/index.html', context)
+    return render(request, 'registration/register.html', context)
