0927a56dd90621599b7de130ce859daafc936998
Icarus <hello@gmail.com>
2020-02-18 13:33:28 -0500

    User auth

--- user/views.py
+++ user/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import Http404
 
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -11,6 +12,17 @@ def login(request):
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
     context = {'form' : form}
     return render(request, 'registration/register.html', context)
