20884c35a7074139c7e00e0c18274c9c08a71c02
Icarus <hello@gmail.com>
2020-02-17 23:04:38 -0500

    Base html path.

--- Quiz/urls.py
+++ Quiz/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('quizApp/', include('quizApp.urls')),
-    path('', include('django.contrib.auth.urls')),
+    path('', include('user.urls')),
 
 ]
