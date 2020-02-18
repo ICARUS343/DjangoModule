19bba7495ad0aa66f9dd8d138821bfa5c8a4932a
pshivako <pshivako@u.rochester.edu>
2020-02-17 19:20:04 -0500

    Login required

--- quizApp/views.py
+++ quizApp/views.py
@@ -8,23 +8,25 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     if not request.user.is_authenticated:
         return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
-    latest_quiz_list = Quiz.objects.order_by('id')[:5]
-    context = {
-        'latest_quiz_list': latest_quiz_list,
-    }
-    return render(request, 'quizApp/quiz.html', context)
+    else:
+        latest_quiz_list = Quiz.objects.order_by('id')[:5]
+        context = {
+            'latest_quiz_list': latest_quiz_list,
+        }
+        return render(request, 'quizApp/quiz.html', context)
 
 
 def question(request, quiz):
     if not request.user.is_authenticated:
         return redirect('%s?next=%s' % (settings.LOGIN_URL, request.path))
-    try:
-        latest_question_list = Question.objects.filter(quiz_foreign_key = quiz)
-        context = {'latest_question_list': latest_question_list}
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    else:
+        try:
+            latest_question_list = Question.objects.filter(quiz_foreign_key = quiz)
+            context = {'latest_question_list': latest_question_list}
+        except Question.DoesNotExist:
+            raise Http404("Question does not exist")
 
-    return render(request, 'quizApp/question.html', context)
+        return render(request, 'quizApp/question.html', context)
 
 def auth(request):
     return render(request, '')
