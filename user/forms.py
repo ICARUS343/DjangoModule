fb92c27bc7f769b64d1f9d61b2291b005c2ad1c6
Icarus <hello@gmail.com>
2020-02-18 19:27:39 -0500

    Access

--- user/forms.py
+++ user/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
 from django.contrib.auth.models import User
 
 
-class SignUpForm(UserCreationForm):
+class SignUpForm(ModelForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     CHOICES = (
