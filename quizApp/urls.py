b91f448b29385e8248781e0b76419bad4b478d31
Icarus <hello@gmail.com>
2020-02-19 01:49:33 -0500

    Access

--- quizApp/urls.py
+++ quizApp/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     #path('<int:quiz_id>/answer/', views.answer_detail, name='answer_detail'),
 
     path('quiz_taker/', views.quiz_taker, name = 'quiz_taker'),
-    path('quiz_admin/', views.quiz_taker, name='quiz_admin')
+    path('quiz_admin/', views.quiz_admin, name='quiz_admin')
 
 ]
