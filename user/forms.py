9d365f37dd565be24c325d365e77559c1dec4923
Icarus <hello@gmail.com>
2020-02-18 22:26:56 -0500

    Access

--- user/forms.py
+++ user/forms.py
@@ -1,14 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+class GeeksForm(forms.Form):
+    geeks_field = forms.ChoiceField(choices = GEEKS_CHOICES)
 
 class SignUpForm(UserCreationForm):
-    user_group = (
+    user_group1 = (
         ("1", "quiz_admin"),
         ("2", "quiz_maker"),
         ("3", "quiz_taker"),
     )
     class Meta:
         model = User
+        user_group = forms.ChoiceField(choices = user_group1)
         fields = ('username', 'user_group', 'password1', 'password2', )
