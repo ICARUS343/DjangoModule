859936de4122c4313ec559860134830da57d6b24
Icarus <hello@gmail.com>
2020-02-19 01:27:16 -0500

    Access

--- quizApp/views.py
+++ quizApp/views.py
@@ -1,22 +1,30 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import Http404
 
 from .models import *
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import Group
 
 
 @login_required(login_url='/accounts/login/')
-@user_passes_test(lambda u: u.groups.filter(name ='quiz_takers' and 'quiz_admins').count() == 0, login_url='/quizApp/quiz_admin')
 def index(request):
-    latest_quiz_list = Quiz.objects.order_by('id')[:5]
-    context = {
-        'latest_quiz_list': latest_quiz_list,
-    }
-    return render(request, 'quizApp/quiz.html', context)
-
+    users_in_admin = Group.objects.get(name="quiz_admins").user_set.all()
+    users_in_taker = Group.objects.get(name="quiz_takers").user_set.all()
+    users_in_maker = Group.objects.get(name="quiz_makers").user_set.all()
+    if request.user in users_in_admin:
+        return redirect('/quizApp/quiz_admin')
+    if request.user in users_in_taker:
+        return redirect('/quizApp/quiz_maker')
+    if request.user in users_in_maker:
+        latest_quiz_list = Quiz.objects.order_by('id')[:5]
+        context = {
+            'latest_quiz_list': latest_quiz_list,
+        }
+        return render(request, 'quizApp/quiz.html', context)
+    else:
+        return render(request, '/access')
 
 @login_required(login_url='/accounts/login/')
-@user_passes_test(lambda u: u.groups.filter(name ='quiz_takers' and 'quiz_admins').count() == 0, login_url='/quizApp/quiz_admin')
 def question(request, quiz):
     try:
         latest_question_list = Question.objects.filter(quiz_foreign_key = quiz)
@@ -28,14 +36,20 @@ def question(request, quiz):
 
 
 @login_required(login_url='/accounts/login/')
-@user_passes_test(lambda u: u.groups.filter(name='quiz_takers' and 'quiz_admin').count() == 0, login_url='/no_zccount')
 def quiz_taker(request):
     return render(request, "quizApp/quiz_taker.html")
 
 
 @login_required(login_url='/accounts/login/')
-@user_passes_test(lambda u: u.groups.filter(name='quiz_makers' and 'takers').count() == 0, login_url='quizApp/quiz_taker')
 def quiz_admin(request):
     return render(request, "quizApp/quiz_admin.html")
 
 
+def is_quiz_taker(user):
+    return user.groups.filter(name='quiz_takers').exists()
+def is_quiz_admin(user):
+    return user.groups.filter(name='quiz_admins').exists()
+def is_quiz_maker(user):
+    return user.groups.filter(name='quiz_makers').exists()
+def is_in_multiple_groups(user):
+    return user.groups.filter(name__in=['group1', 'group2']).exists()
