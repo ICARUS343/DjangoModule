12fcea7294bc9e1f40428256ec4f7bdbd202ec3c
Icarus <hello@gmail.com>
2020-02-18 19:49:05 -0500

    Access

--- user/forms.py
+++ user/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
-    user_group = forms.CharField(label = "Choose your authentication level", max_length=10)
+    user_group = forms.CharField(label = "<br/>Choose your authentication level", max_length=10)
