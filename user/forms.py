e0f9f4b0688d61fcbc30ec615a48de25413c2a50
Icarus <hello@gmail.com>
2020-02-18 22:24:59 -0500

    Access

--- user/forms.py
+++ user/forms.py
@@ -4,8 +4,11 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    user_group = forms.EmailField(max_length=254, help_text='Required. Inform a valid user group.(quiz_admin, quiz_maker, quiz_taker)')
-
+    user_group = (
+        ("1", "quiz_admin"),
+        ("2", "quiz_maker"),
+        ("3", "quiz_taker"),
+    )
     class Meta:
         model = User
         fields = ('username', 'user_group', 'password1', 'password2', )
