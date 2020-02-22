8141763d3c145013d2c3c9a0875dcd3202c16c8f
Icarus <hello@gmail.com>
2020-02-22 16:37:09 -0500

    Looks.

--- user/forms.py
+++ user/forms.py
@@ -11,4 +11,4 @@ class RegisterForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    user_group = forms.ChoiceField(label ='', choices = user_group1, widget=forms.Select(attrs={'class':'form-control'})
+    user_group = forms.ChoiceField(label ='', choices = user_group1, widget=forms.Select(attrs={'class':'form-control'}))
