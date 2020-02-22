770cc5677dade7f73080e78a20d6b634ee6fbe99
Icarus <hello@gmail.com>
2020-02-22 16:36:34 -0500

    Looks.

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from forms import *
+
 user_group1 = (
         ("quiz_admins", "quiz_admins"),
         ("quiz_makers", "quiz_makers"),
@@ -11,4 +11,4 @@ class RegisterForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    user_group = ChoiceField(label ='', choices = user_group1, widget=forms.Select(attrs={'class':'form-control'})
+    user_group = forms.ChoiceField(label ='', choices = user_group1, widget=forms.Select(attrs={'class':'form-control'})
