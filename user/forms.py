6f4024afa063ef544247e026e3cc257e961e7b8a
Icarus <hello@gmail.com>
2020-02-26 10:47:01 -0500

    Removed unwanted '/'

--- user/forms.py
+++ user/forms.py
@@ -7,7 +7,7 @@ class SignUpForm(UserCreationForm):
     user_group1 = (
         ("quiz_admins", "quiz_admins"),
         ("quiz_makers", "quiz_makers"),
-        ("quiz_takers", "quiz_takers"),
+        ("quiz_takerssadasd", "quiz_takers"),
     )
     user_group = forms.MultipleChoiceField(choices=user_group1, widget=forms.CheckboxSelectMultiple)
     class Meta:
