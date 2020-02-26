06c2fe70e3383147433b196ef347fe9bfbd5a49f
Icarus <hello@gmail.com>
2020-02-26 14:54:17 -0500

    hkjh˚

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.models import Group
 from django.shortcuts import get_object_or_404
-from .forms import SignUpForm
+from .forms import RegisterForm
 
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
@@ -22,20 +22,21 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
-        form = SignUpForm(request.POST, request.FILES)
+        form = RegisterForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             username1 = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username1, password=raw_password)
             user_group = form.cleaned_data.get('user_group')
-            g = Group.objects.get(name=user_group)
-            g.user_set.add(user)
-            user.groups.add(g)
+            for assignment in user_group:
+                g = Group.objects.get(name=assignment)
+                g.user_set.add(user)
+                user.groups.add(g)
             auth_login(request, user)
             return redirect('/quizApp/')
     else:
-        form = SignUpForm()
+        form = RegisterForm()
     return render(request, 'registration/register.html', {'form': form})
 
 
