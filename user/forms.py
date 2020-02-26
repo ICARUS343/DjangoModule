51ae0e3acadcb45e51a4f4ec5a4e2810ccb45fe7
Icarus <hello@gmail.com>
2020-02-26 14:04:29 -0500

    sads

--- user/forms.py
+++ user/forms.py
@@ -1,22 +1,16 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-user_group1 = (
+
+class SignUpForm(UserCreationForm):
+    user_group1 = (
         ("quiz_admins", "quiz_admins"),
         ("quiz_makers", "quiz_makers"),
         ("quiz_takers", "quiz_takers"),
     )
-<<<<<<< HEAD
     user_group = forms.MultipleChoiceField(choices=user_group1, widget=forms.CheckboxSelectMultiple)
 
     class Meta:
         model = User
         fields = ('username', 'user_group', 'password1', 'password2',)
-=======
-
-
-class RegisterForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    user_group = forms.ChoiceField(label ='', choices = user_group1, widget=forms.Select(attrs={'class':'form-control'}))
->>>>>>> 78396728bf2e735de201865a0c9f8c355e162a4e
