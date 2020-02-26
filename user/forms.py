5f0fcbb0999e19e5b06f97574ec9d2a5ffb934ed
Icarus <hello@gmail.com>
2020-02-26 10:48:06 -0500

    Removed unwanted '/'

--- user/forms.py
+++ user/forms.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     user_group1 = (
-        ("quiz_admins", "quiz_admins"),
-        ("quiz_makers", "quiz_makers"),
-        ("quiz_takerssadasd", "quiz_takers"),
+        ("quiz_admins", "quiz_admins ()"),
+        ("quiz_makers", "quiz_makers ()"),
+        ("quiz_takers", "quiz_takers ()"),
     )
     user_group = forms.MultipleChoiceField(choices=user_group1, widget=forms.CheckboxSelectMultiple)
     class Meta:
