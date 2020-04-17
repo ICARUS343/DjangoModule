a53115498570b558298ed11652f0a9542bc567b9
Icarus <hello@gmail.com>
2020-04-17 16:51:16 -0400

    das

--- user/views.py
+++ user/views.py
@@ -34,7 +34,7 @@ def register(request):
                 g.user_set.add(user)
                 user.groups.add(g)
             auth_login(request, user)
-            return redirect('')
+            return redirect('quizApp/')
     else:
         form = SignUpForm()
     return render(request, 'registration/register.html', {'form': form})
