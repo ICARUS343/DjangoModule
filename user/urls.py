998f6c423f9a01346a07190ec121ce162563952a
Icarus <hello@gmail.com>
2020-04-17 16:22:02 -0400

    das

--- user/urls.py
+++ user/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('register/', views.register, name='register'),
+    path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
     path('access', views.access_denied, name='access'),
 
